8670d67d858ab79ab9ba5a4052e96a088cbe948d
Jaewon <logix5623@naver.com>
2021-05-27 00:16:32 +0900

    Shader Uniform 변수 세팅 함수 추가

--- Shader/shader.h
+++ Shader/shader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -25,6 +26,17 @@ public:
 	void setBool(const std::string& name, bool value) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
+
+	void setVec2(const std::string& name, float x, float y) const;
+	void setVec3(const std::string& name, float x, float y, float z) const;
+	void setVec4(const std::string& name, float x, float y, float z, float w) const;
+	void setVec2(const std::string& name, const glm::vec2& vec) const;
+	void setVec3(const std::string& name, const glm::vec3& vec) const;
+	void setVec4(const std::string& name, const glm::vec4& vec) const;
+
+	void setMat2(const std::string& name, const glm::mat2& mat) const;
+	void setMat3(const std::string& name, const glm::mat3& mat) const;
+	void setMat4(const std::string& name, const glm::mat4& mat) const;
 
 private:
 	void shaderCompile(const char* vertexCode, const char* framentCode, const char* geometryCode = nullptr);
